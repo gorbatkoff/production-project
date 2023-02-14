f7422401d08dd98b0c1710410b55ac442a2f6c16
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-14 06:44:16 +0300

    Create UI-Button Component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,31 @@
+import {classNames} from "shared/lib/classNames/classNames";
+
+import styles from './Button.module.scss';
+import {ButtonHTMLAttributes, FC} from "react";
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string,
+    theme?: ThemeButton,
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+
+    const {
+        className,
+        children,
+        theme,
+        ...otherProps
+    } = props;
+
+    return (
+        <button className={classNames(styles.Button, {}, [className, styles[theme]])}
+                {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};

9ac4b42394ae4516d0b01ae6fb5aa9d4d0229c1e
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-14 06:43:16 +0300

    Add settings of import and paths

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,8 +1,13 @@
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): webpack.ResolveOptions{
+export function buildResolvers(options: BuildOptions): webpack.ResolveOptions{
     return {
         extensions: ['.tsx', '.ts', '.js'], // Указываем расширения тех файлов, при импорте которых
         // мы не будем указывать расширение (import Component from './component')
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'], // импорты из этой папки явл. абсолютными
+        mainFiles: ['index'],
+        alias: {}
     }
 }

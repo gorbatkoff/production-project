3c43371b9945c33a7d11daa561233ad96f0cc9b3
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-14 06:46:01 +0300

    Public API

---
+++ src/app/providers/ThemeProvider/index.ts
@@ -0,0 +1,9 @@
+import ThemeProvider from "./ui/ThemeProvider";
+import {useTheme} from "./lib/useTheme";
+import {Theme} from "./lib/themeContext";
+
+export {
+    ThemeProvider,
+    useTheme,
+    Theme
+}
---
+++ src/widgets/Navbar/index.ts
@@ -0,0 +1,5 @@
+import {Navbar} from "widgets/Navbar/ui/Navbar";
+
+export {
+    Navbar
+}

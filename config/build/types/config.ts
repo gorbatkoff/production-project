1eb59d816ae2845c5dbafa420a19ac8e86b08299
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-14 06:45:28 +0300

    Add key of interface with path to folder with initial code

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -4,6 +4,7 @@ export interface BuildPaths {
     entry: string; // Путь до entry поинта
     build: string; // До папки со сборкой
     html: string; // До html файла (public)
+    src: string; // Путь до папки с исходным кодом
 }
 
 export interface BuildEnv {

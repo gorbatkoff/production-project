fa94eccdb2efe1e0f41926c18d58f8a3b4c4864f
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-16 09:43:56 +0300

    Create Sidebar component

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import {classNames} from "shared/lib/classNames/classNames";
+
+import styles from './Sidebar.module.scss';
+import {useState} from "react";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
+
+interface SidebarProps {
+    className?: string
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+
+    const [collapsed, setCollapsed] = useState(false)
+
+    function onToggle() {
+        setCollapsed(prev => !prev);
+    }
+
+    return (
+        <div className={
+            classNames(styles.Sidebar,
+                {[styles.collapsed]: collapsed},
+                [className])}
+        >
+            <button onClick={onToggle}>
+                Toggle
+            </button>
+
+            <div className={styles.switchers}>
+                <ThemeSwitcher/>
+                <LangSwitcher className={styles.lang}/>
+            </div>
+        </div>
+    );
+};

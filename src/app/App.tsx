2b5ac6bfed8a94c0db009c9d93371a0bcb5d52ea
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-16 09:42:19 +0300

    Add Suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,29 @@
 import {Suspense, useContext, useState} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+
 import './styles/index.scss'
+
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+
+import {useTranslation} from "react-i18next";
+
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <AppRouter />
+            <Suspense fallback="Loading...">
+                <Navbar/>
+                <div className="content-page">
+
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };

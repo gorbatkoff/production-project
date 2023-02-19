fa80c6c6fb7fde5986d3ff7a00c6fd7ca8a03649
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-19 16:59:59 +0300

    Add Suspense for route's element

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,25 @@
 import React, {Suspense} from "react"
 import {Route, Routes} from "react-router-dom"
 import {routeConfig} from "shared/config/routeConfig/routeConfig"
+import {PageLoader} from "widgets/PageLoader";
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(r => (
-                <Route
-                    key={r.path}
-                    path={r.path}
-                    element={
-                        <div className='page-wrapper'>
-                            {r.element}
-                        </div>
-                    }
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <Routes>
+        {Object.values(routeConfig).map(r => (
+            <Route
+                key={r.path}
+                path={r.path}
+                element={
+                    (
+                        <Suspense fallback={<PageLoader />}>
+                            <div className='page-wrapper'>
+                                {r.element}
+                            </div>
+                        </Suspense>
+                    )}
+            />
+        ))}
+    </Routes>
 )
 
 export default AppRouter

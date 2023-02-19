54100dee1f8e7e2c0455ae052339c445d39bc4ba
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-19 16:56:51 +0300

    Add route for NotFoundPage

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,17 +1,20 @@
 // Храним информацию о маршрутах в перечислениях
-import {type RouteProps} from "react-router-dom"
+import {RouteProps} from "react-router-dom"
 import {MainPage} from "pages/MainPage"
 import {AboutPage} from "pages/AboutPage"
+import {NotFoundPage} from "pages/NotFoundPage";
 
 export enum AppRoutes {
 	MAIN = "main",
 	ABOUT = "about",
+	NOT_FOUND = "not_found",
 }
 
 // Тут из enum/a мы указываем путь до соответствующего компонента
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: "/",
     [AppRoutes.ABOUT]: "/about",
+    [AppRoutes.NOT_FOUND]: "*",
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -22,5 +25,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage/>,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage/>,
     },
 }

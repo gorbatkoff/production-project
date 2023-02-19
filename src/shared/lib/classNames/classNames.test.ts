59ca28bfbb33c0ee75ae1d42e6cf506620ac8e03
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-19 11:30:14 +0300

    Add unit tests for classNames function

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,33 @@
+import {classNames} from "./classNames"
+
+
+describe("classNames testing", () => {
+    test("Only one first parameter", () => {
+        expect(classNames("banner", {}, [])).toBe("banner")
+    })
+    test("Also with additional parameters", () => {
+        expect(classNames("banner",
+            {},
+            ["cls1", "cls2"])).toBe("banner cls1 cls2")
+    })
+    test("Also with mods and additional parameters", () => {
+        expect(classNames("banner",
+            {hover: true, clicked: true},
+            ["cls1", "cls2"])).toBe("banner cls1 cls2 hover clicked")
+    })
+    test("Mods have falsy values", () => {
+        expect(classNames("banner",
+            {"hover": false, "clicked": true},
+            ["cls1", "cls2"])).toBe("banner cls1 cls2 clicked")
+    })
+    test("Mods have only falsy values", () => {
+        expect(classNames("banner",
+            {"hover": false, "clicked": false},
+            ["cls1", "cls2"])).toBe("banner cls1 cls2")
+    })
+    test("Any values", () => {
+        expect(classNames("banner",
+            {"hover": true, "clicked": true},
+            ["cls1", undefined, null, ""])).toBe("banner cls1 hover clicked")
+    })
+});

5fda55e0ebc02667ad0137ff141e8cb6d7245563
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-24 21:47:54 +0300

    Add decorators for storybook

---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import {addDecorator} from "@storybook/react";
+import {StyleDecorator} from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+import {ThemeDecorator} from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "../../src/app/providers/ThemeProvider";
+import {RouterDecorator} from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+
+export const parameters = {
+    actions: {argTypesRegex: "^on[A-Z].*"},
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(StyleDecorator); // Добавление декоратора со стилями
+addDecorator(ThemeDecorator(Theme.LIGHT)); // Добавление стилей отностительно компонента
+addDecorator(RouterDecorator); // Добавление роутинга для компонентов в которых есть ссылка

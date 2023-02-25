4249d442204bb2724618c6993e7ffab38c1978a4
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-25 20:51:41 +0300

    *CHANGED ThemeButton -> ButtonTheme

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import {classNames} from "shared/lib/classNames/classNames"
 
 import IconThemeSwitcher from "shared/assets/images/IconThemeSwitcher.svg"
 
-import {Button, ThemeButton} from "shared/ui/Button/Button"
+import {Button, ButtonTheme} from "shared/ui/Button/Button"
 
 type ThemeSwitcherProps = {
 	className?: string;
@@ -14,7 +14,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
 
     return (
         <Button
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             className={classNames("", {}, [className])}
             onClick={toggleTheme}
         >

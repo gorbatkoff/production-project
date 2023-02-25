c26cdf3d06c172e0c5c7010a8b0ceb901999dd51
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-25 20:49:28 +0300

    Delete links from Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames"
 
 import styles from "./Navbar.module.scss"
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink"
 import {useTranslation} from "react-i18next";
 
 type NavbarProps = {
@@ -15,15 +14,7 @@ export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(styles.Navbar, {}, [])}>
 
-            <div className={styles.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={"/"} className={styles.mainLink}>
-                    {t("Главная")}
-                </AppLink>
-
-                <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-                    {t("О сайте")}
-                </AppLink>
-            </div>
+            <div className={styles.links}>/</div>
         </div>
     )
 }

d1daad8a7bdd7f9f73b760f02d31c2f035250c5f
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-28 15:29:28 +0300

    Add ThemeDecorator wrapper

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,11 +1,13 @@
 import {Story} from "@storybook/react";
-import {Theme} from "app/providers/ThemeProvider";
+import {Theme, ThemeProvider} from "app/providers/ThemeProvider";
 
 // eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
     return (
-        <div className={`app ${theme}`}>
-            <StoryComponent/>
-        </div>
+        <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>
+                <StoryComponent/>
+            </div>
+        </ThemeProvider>
     )
 }

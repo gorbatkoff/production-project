6416387465d5c11e48da04f017413bd5172e9d8d
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-28 15:28:33 +0300

    Add Modal and log in button

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,9 @@ import {classNames} from "shared/lib/classNames/classNames"
 
 import styles from "./Navbar.module.scss"
 import {useTranslation} from "react-i18next";
+import {Modal} from "shared/ui/Modal/Modal";
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
+import {useCallback, useState} from "react";
 
 type NavbarProps = {
     className?: string;
@@ -11,10 +14,24 @@ export const Navbar = ({className}: NavbarProps) => {
 
     const {t} = useTranslation()
 
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal(prev => !prev)
+    }, [])
     return (
         <div className={classNames(styles.Navbar, {}, [])}>
 
-            <div className={styles.links}>/</div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={styles.links}
+                onClick={onToggleModal}
+            >
+                {t("Войти")}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {t("Lorem")}
+            </Modal>
         </div>
     )
 }

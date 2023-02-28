23d47ee85315943a0aeda3a612486826e11512ac
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-28 15:23:41 +0300

    Add Clear_Inverted theme

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -6,6 +6,7 @@ import {ButtonHTMLAttributes, FC} from "react"
 
 export enum ButtonTheme {
     CLEAR = "clear",
+    CLEAR_INVERTED = "clearInverted",
     OUTLINE = "outline",
     BACKGROUND = "background",
     BACKGROUND_INVERTED = "backgroundInverted",

91fb95d43696d37c9eed964ce9ee37ad6cfe594f
gorbatkoff <gorbatkovartyom@gmail.com>
2023-02-28 15:22:48 +0300

    Delete import of global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,21 @@
-import {Suspense, useEffect} from "react"
-
-import "./styles/index.scss"
+import {Suspense, useEffect, useState} from "react"
 
 import {classNames} from "shared/lib/classNames/classNames"
 import {useTheme} from "app/providers/ThemeProvider"
 import {AppRouter} from "app/providers/router"
 import {Navbar} from "widgets/Navbar"
 import {Sidebar} from "widgets/Sidebar"
+import {Modal} from "shared/ui/Modal/Modal";
 
 const App = () => {
     const {theme} = useTheme();
+
 
     return (
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback='Loading...'>
                 <Navbar/>
+                {/*<button onClick={() => setIsOpen(true)}>toggle</button>*/}
                 <div className='content-page'>
                     <Sidebar/>
                     <AppRouter/>

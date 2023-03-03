7dba08b0f7c7250456c60ee15eb10dfd1d719f00
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-03 19:00:31 +0300

    Some fixes

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,3 @@
-import {classNames} from "shared/lib/classNames/classNames";
-
-import styles from "./LoginModal.module.scss";
 import {Modal} from "shared/ui/Modal/Modal";
 import {LoginForm} from "../LoginForm/LoginForm";
 
@@ -12,7 +9,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({className, isOpen, onClose}: LoginModalProps) => {
     return (
-        <Modal className={classNames(styles.LoginModal, {}, [className])}
+        <Modal
             isOpen={isOpen} onClose={onClose}
         >
             <LoginForm/>

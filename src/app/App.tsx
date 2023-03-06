a52dc4738266c5ddcef521ce7bdeecdd8f826b87
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:08:09 +0300

    Add dispatch on initAuthData

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,21 @@
-import {Suspense, useEffect, useState} from "react"
+import {Suspense, useEffect} from "react"
 
 import {classNames} from "shared/lib/classNames/classNames"
-import {useTheme} from "app/providers/ThemeProvider"
-import {AppRouter} from "app/providers/router"
+import {useTheme} from "./providers/ThemeProvider"
+import {AppRouter} from "./providers/router"
 import {Navbar} from "widgets/Navbar"
 import {Sidebar} from "widgets/Sidebar"
-import {Modal} from "shared/ui/Modal/Modal";
+import {useDispatch} from "react-redux";
+import {userActions} from "entities/User";
 
 const App = () => {
     const {theme} = useTheme();
 
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(userActions.initAuthData())
+    }, [dispatch])
 
     return (
         <div className={classNames("app", {}, [theme])}>

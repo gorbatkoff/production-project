9ad5ea5eca6072cee01c592579e77d473591d9a1
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:14:44 +0300

    Add loginForm and loginReducer

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,12 +2,14 @@ import {configureStore, ReducersMapObject} from "@reduxjs/toolkit"
 import {StateSchema} from "./StateSchema";
 import {counterReducer} from "entities/Counter";
 import {userReducer} from "entities/User";
+import {loginReducer} from "features/AuthByUsername";
 
 export function createReduxStore(initialState?: StateSchema) {
 
     const rootReducer: ReducersMapObject<StateSchema> = {
         counter: counterReducer,
         user: userReducer,
+        loginForm: loginReducer
     };
 
     return configureStore<StateSchema>({

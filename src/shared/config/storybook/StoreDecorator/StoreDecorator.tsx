ee34d0fb4d196412332d35ed74c842116fa8bcb0
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:20:10 +0300

    Create StoreDecorator for storybook

---
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -0,0 +1,11 @@
+import {Story} from "@storybook/react";
+import {StateSchema, StoreProvider} from "app/providers/StoreProvider";
+import {DeepPartial} from "@reduxjs/toolkit";
+
+export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => {
+    return (
+        <StoreProvider initialState={state}>
+            <StoryComponent/>
+        </StoreProvider>
+    )
+}

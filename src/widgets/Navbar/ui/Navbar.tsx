bed236e1af4be5adbcc6c073d5d608a272df957c
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:12:53 +0300

    Add logic for Navbar component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,11 +2,11 @@ import {classNames} from "shared/lib/classNames/classNames"
 
 import styles from "./Navbar.module.scss"
 import {useTranslation} from "react-i18next";
-import {Modal} from "shared/ui/Modal/Modal";
 import {Button, ButtonTheme} from "shared/ui/Button/Button";
 import {useCallback, useState} from "react";
-import {LoginForm} from "features/AuthByUsername/ui/LoginForm/LoginForm";
 import {LoginModal} from "features/AuthByUsername";
+import {useDispatch, useSelector} from "react-redux";
+import {getUserAuthData, userActions} from "entities/User";
 
 type NavbarProps = {
     className?: string;
@@ -17,6 +17,8 @@ export const Navbar = ({className}: NavbarProps) => {
     const {t} = useTranslation()
 
     const [isAuthModal, setIsAuthModal] = useState(false);
+    const dispatch = useDispatch()
+    const authData = useSelector(getUserAuthData);
 
     const onCloseModal = useCallback(() => {
         setIsAuthModal(false)
@@ -25,6 +27,25 @@ export const Navbar = ({className}: NavbarProps) => {
     const onShowModal = useCallback(() => {
         setIsAuthModal(true)
     }, [])
+
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout())
+    }, [dispatch])
+
+
+    if (authData) {
+        return (
+            <div className={classNames(styles.Navbar, {}, [])}>
+                <Button
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    className={styles.links}
+                    onClick={onLogout}
+                >
+                    {t("Выйти")}
+                </Button>
+            </div>
+        )
+    }
 
     return (
         <div className={classNames(styles.Navbar, {}, [])}>

e7e75e0028326dc14f28ccd89ae280850f25e18e
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:12:34 +0300

    Add decorators for Navbar.stories.tsx

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -4,6 +4,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 import { Navbar } from "./Navbar";
+import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 export default {
     title: "widget/Navbar",
@@ -17,7 +18,13 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
+Dark.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
 Dark.decorators = [ThemeDecorator(Theme.DARK)];

beec164bed1331688c22636ef1f1116477cff9f6
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:21:01 +0300

    Create story-cases for Text (UI component)

---
+++ src/shared/ui/Text/Text.stories.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {ComponentStory, ComponentMeta} from "@storybook/react";
+
+import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "app/providers/ThemeProvider";
+import {Text, TextTheme} from "./Text";
+
+export default {
+    title: "shared/Text",
+    component: Text,
+    argTypes: {
+        backgroundColor: {control: "color"},
+    },
+} as ComponentMeta<typeof Text>;
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const WithTitle = Template.bind({});
+WithTitle.args = {
+    title: "Title lorem ipsum dolor sit amet"
+};
+
+export const WithTitleDark = Template.bind({});
+WithTitleDark.args = {
+    title: "Title lorem ipsum dolor sit amet"
+};
+WithTitleDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const WithDescription = Template.bind({});
+WithDescription.args = {
+    description: "Description lorem ipsum dolor sit amet"
+};
+
+export const WithDescriptionDark = Template.bind({});
+WithDescriptionDark.args = {
+    description: "Description lorem ipsum dolor sit amet"
+};
+WithDescriptionDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+
+export const TitleAndDescription = Template.bind({});
+TitleAndDescription.args = {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    description: "Description lorem ipsum dolor sit amet"
+};
+
+export const TitleAndDescriptionDark = Template.bind({});
+TitleAndDescriptionDark.args = {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    description: "Description lorem ipsum dolor sit amet"
+};
+TitleAndDescriptionDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const ErrorTheme = Template.bind({});
+ErrorTheme.args = {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    description: "Description lorem ipsum dolor sit amet",
+    theme: TextTheme.ERROR
+};
+
+export const ErrorThemeDark = Template.bind({});
+ErrorThemeDark.args = {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    description: "Description lorem ipsum dolor sit amet",
+    theme: TextTheme.ERROR
+};
+ErrorThemeDark.decorators = [ThemeDecorator(Theme.DARK)];

05f9a7d99d186a37de64fa65dc3c4fa4650912b4
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:16:07 +0300

    Create reducers for userSlice

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,12 +1,29 @@
-import {createSlice} from "@reduxjs/toolkit"
-import {UserChema} from "../types/user";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit"
+import {User, UserSchema} from "../types/user";
+import {USER_LOCALSTORAGE_KEY} from "shared/const/localStorage";
 
-const initialState:UserChema = {}
+const initialState:UserSchema = {}
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {},
+    reducers: {
+        setAuthData: (state, action:PayloadAction<User>) => {
+            state.authData = action.payload
+        },
+
+        initAuthData: (state, action:PayloadAction<User>) => {
+            const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+            if(user){
+                state.authData = JSON.parse(user)
+            }
+        },
+
+        logout: (state, action:PayloadAction<User>) => {
+            state.authData = undefined;
+            localStorage.removeItem(USER_LOCALSTORAGE_KEY)
+        },
+    },
 })
 
 // Action creators are generated for each case reducer function

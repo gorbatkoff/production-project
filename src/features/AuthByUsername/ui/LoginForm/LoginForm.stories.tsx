f254011d34d3450d1cd3d91278cd2b1d45909ccd
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:11:40 +0300

    Add stories for LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -4,6 +4,7 @@ import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "app/providers/ThemeProvider";
 import {LoginForm} from "./LoginForm";
+import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 
 export default {
@@ -18,8 +19,30 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 
 export const LoginFormLight = Template.bind({});
 LoginFormLight.args = {};
+LoginFormLight.decorators = [StoreDecorator({
+    loginForm: {username: "123", password: "qwerty",}
+})]
 
 export const LoginFormDark = Template.bind({});
 LoginFormDark.args = {};
-LoginFormDark.decorators = [ThemeDecorator(Theme.DARK)];
+LoginFormDark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    loginForm: {username: "123", password: "qwerty",}
+})];
 
+export const LoginFormWithError = Template.bind({});
+LoginFormWithError.args = {};
+LoginFormWithError.decorators = [StoreDecorator({
+    loginForm: {username: "123", password: "qwerty", error: "Неверный логин или пароль"}
+})]
+
+export const LoginFormWithErrorDark = Template.bind({});
+LoginFormWithErrorDark.args = {};
+LoginFormWithErrorDark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    loginForm: {username: "123", password: "qwerty", error: "Неверный логин или пароль"}
+})];
+
+export const WithLoading = Template.bind({});
+WithLoading.args = {};
+WithLoading.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    loginForm: {isLoading: true}
+})];

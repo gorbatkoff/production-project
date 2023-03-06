7d2e4aa3d1e643908f1f4676f6e6ba91884e8f40
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:49:34 +0300

    Update

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,6 @@ import {StyleDecorator} from "../../src/shared/config/storybook/StyleDecorator/S
 import {ThemeDecorator} from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "../../src/app/providers/ThemeProvider";
 import {RouterDecorator} from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
-import {TranslationDecorator} from "../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator";
 
 export const parameters = {
     actions: {argTypesRegex: "^on[A-Z].*"},
@@ -16,6 +15,5 @@ export const parameters = {
 };
 
 addDecorator(StyleDecorator); // Добавление декоратора со стилями
-// addDecorator(TranslationDecorator)
 addDecorator(ThemeDecorator(Theme.LIGHT)); // Добавление стилей отностительно компонента
 addDecorator(RouterDecorator); // Добавление роутинга для компонентов в которых есть ссылка

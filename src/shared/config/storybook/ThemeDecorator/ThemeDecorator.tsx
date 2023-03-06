98f3891ea61a86ecd4c4040ca273c5226352f18b
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:15:14 +0300

    Delete useless eslint-disable-next-line

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,7 +1,6 @@
 import {Story} from "@storybook/react";
 import {Theme, ThemeProvider} from "app/providers/ThemeProvider";
 
-// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
     return (
         <ThemeProvider initialTheme={theme}>

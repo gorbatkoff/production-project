bab2b574b62bc180c9a381436430bfd4ab419d02
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:26:05 +0300

    Configure translation files

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,22 +1,24 @@
-import i18n from "i18next"
-import {initReactI18next} from "react-i18next"
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-http-backend"
-import LanguageDetector from "i18next-browser-languagedetector"
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const  __IS_DEV__ = false; // hotfix, need to change by lesson
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        lng: "ru",
-        fallbackLng: "ru",
+        fallbackLng: "en",
         debug: __IS_DEV__,
 
         interpolation: {
-            escapeValue: false, // Not needed for react as it escapes by default
+            escapeValue: false, // not needed for react as it escapes by default
         },
-    })
 
-export default i18n
+        backend: {
+            loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+    });
+
+export default i18n;

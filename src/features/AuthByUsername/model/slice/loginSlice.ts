a344bff9b26add98fd000fcda9ac2ecf90082306
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:19:55 +0300

    Create loginSlice

---
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -0,0 +1,45 @@
+import {createSlice} from "@reduxjs/toolkit"
+import type {PayloadAction} from "@reduxjs/toolkit"
+import {LoginSchema} from "../types/loginSchema";
+import {loginByUsername} from "../services/loginByUsername/loginByUsername";
+
+const initialState: LoginSchema = {
+    username: "",
+    password: "",
+    isLoading: false,
+}
+
+export const loginSlice = createSlice({
+    name: "login",
+    initialState,
+    reducers: {
+        setUsername: (state, action: PayloadAction<string>) => {
+            state.username = action.payload
+        },
+
+        setPassword: (state, action: PayloadAction<string>) => {
+            state.password = action.payload
+        },
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginByUsername.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(loginByUsername.fulfilled, (state,
+                action) => {
+                state.isLoading = false;
+            })
+            .addCase(loginByUsername.rejected, (state,
+                action) => {
+                state.isLoading = false;
+                state.error = action.payload
+            })
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const {actions: loginActions} = loginSlice;
+export const {reducer: loginReducer} = loginSlice;

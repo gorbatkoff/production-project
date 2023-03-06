4cc5db6efd80ca877d2bf281ef53c742a8c52353
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-06 21:17:30 +0300

    CHANGE config.resolve.modules.PUSH to config.resolve.modules.PUSH ! IT'S BUG OF STORYBOOK https://github.com/storybookjs/storybook/issues/21431

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack, {RuleSetRule} from "webpack";
+import webpack, {DefinePlugin, RuleSetRule} from "webpack";
 import {BuildPaths} from "../build/types/config";
 import path from "path";
 import {buildCssLoaders} from "../build/loaders/buildCssLoaders";
@@ -11,7 +11,7 @@ export default ({config}: { config: webpack.Configuration }) => {
         html: "",
         src: path.resolve(__dirname, "..", "..", "src")
     }
-    config.resolve.modules.push(paths.src);
+    config.resolve.modules.unshift(paths.src);
     config.resolve.extensions.push(".ts", ".tsx");
 
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
@@ -28,6 +28,10 @@ export default ({config}: { config: webpack.Configuration }) => {
         use: ["@svgr/webpack"],
     })
     config.module.rules.push(buildCssLoaders(true))
+
+    config.plugins.push(new DefinePlugin({
+        __IS_DEV__: true
+    }))
 
     return config;
 };

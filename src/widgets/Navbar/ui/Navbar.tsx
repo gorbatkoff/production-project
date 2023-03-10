585506290f5d081c16a202370d91787d32c3251c
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-10 20:47:34 +0300

    Add deleting LoginModal from DOM by condition

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -58,10 +58,10 @@ export const Navbar = ({className}: NavbarProps) => {
                 {t("Войти")}
             </Button>
 
-            <LoginModal
+            {isAuthModal && <LoginModal
                 isOpen={isAuthModal}
                 onClose={onCloseModal}
-            />
+            />}
         </div>
     )
 }

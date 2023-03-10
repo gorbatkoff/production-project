fd3f3ef76dce04c4d957c921781e793c90ebdc0d
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-10 20:45:28 +0300

    Add Suspense for LoginForm

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,18 +1,24 @@
-import {Modal} from "shared/ui/Modal/Modal";
-import {LoginForm} from "../LoginForm/LoginForm";
+import { Modal } from 'shared/ui/Modal/Modal';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Suspense } from 'react';
+import { Loader } from 'shared/ui/Loader/Loader';
+import { LoginFormAsync } from '../LoginForm/LoginForm.async';
 
 interface LoginModalProps {
     className?: string;
     isOpen: boolean;
-    onClose: () => void
+    onClose: () => void;
 }
 
-export const LoginModal = ({className, isOpen, onClose}: LoginModalProps) => {
-    return (
-        <Modal
-            isOpen={isOpen} onClose={onClose}
-        >
-            <LoginForm/>
-        </Modal>
-    );
-};
+export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
+    <Modal
+        className={classNames("", {}, [className])}
+        isOpen={isOpen}
+        onClose={onClose}
+        lazy
+    >
+        <Suspense fallback={<Loader />}>
+            <LoginFormAsync />
+        </Suspense>
+    </Modal>
+);

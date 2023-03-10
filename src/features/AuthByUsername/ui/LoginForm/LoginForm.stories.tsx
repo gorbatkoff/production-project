130faa64de23aeef807541359e9a7b072652a094
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-10 20:46:11 +0300

    Change type of import LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -3,7 +3,7 @@ import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "app/providers/ThemeProvider";
-import {LoginForm} from "./LoginForm";
+import LoginForm from "./LoginForm";
 import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 

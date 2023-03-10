de1f81bbbf59727bf2a02ad0761c94e85fedfb0c
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-10 20:44:31 +0300

    Index API

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,9 +1,7 @@
 import {LoginModal} from "./ui/LoginModal/LoginModal";
 import {LoginSchema} from "./model/types/loginSchema";
-import {loginReducer} from "./model/slice/loginSlice";
 
 export {
     LoginModal,
     LoginSchema,
-    loginReducer
 }

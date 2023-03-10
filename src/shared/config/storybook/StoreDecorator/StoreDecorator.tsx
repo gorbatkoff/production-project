99aa9581b8c7a72917ef674279086e2ffa5d2b1d
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-10 20:49:08 +0300

    Add defaultAsyncReducers

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,10 +1,18 @@
 import {Story} from "@storybook/react";
 import {StateSchema, StoreProvider} from "app/providers/StoreProvider";
-import {DeepPartial} from "@reduxjs/toolkit";
+import {DeepPartial, ReducersMapObject} from "@reduxjs/toolkit";
+import {loginReducer} from "features/AuthByUsername/model/slice/loginSlice";
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => {
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+}
+
+export const StoreDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+) => (StoryComponent: Story) => {
     return (
-        <StoreProvider initialState={state}>
+        <StoreProvider initialState={state} asyncReducers={{...defaultAsyncReducers, ...asyncReducers}}>
             <StoryComponent/>
         </StoreProvider>
     )

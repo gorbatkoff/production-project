9e664afcf73b606c32060fbe0f68e0658c7be0fc
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-11 20:03:02 +0300

    Add new route (profile)

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -3,17 +3,22 @@ import {RouteProps} from "react-router-dom"
 import {MainPage} from "pages/MainPage"
 import {AboutPage} from "pages/AboutPage"
 import {NotFoundPage} from "pages/NotFoundPage";
+import {ProfilePage} from "pages/ProfilePage";
 
 export enum AppRoutes {
-	MAIN = "main",
-	ABOUT = "about",
-	NOT_FOUND = "not_found",
+    MAIN = "main",
+    ABOUT = "about",
+    PROFILE = "profile",
+
+    //last
+    NOT_FOUND = "not_found",
 }
 
 // Тут из enum/a мы указываем путь до соответствующего компонента
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: "/",
     [AppRoutes.ABOUT]: "/about",
+    [AppRoutes.PROFILE]: "/profile",
     [AppRoutes.NOT_FOUND]: "*",
 }
 
@@ -25,6 +30,10 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage/>,
+    },
+    [AppRoutes.PROFILE]: {
+        path: RoutePath.profile,
+        element: <ProfilePage/>,
     },
     [AppRoutes.NOT_FOUND]: {
         path: RoutePath.not_found,

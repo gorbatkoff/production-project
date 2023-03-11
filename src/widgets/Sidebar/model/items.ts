ba694b7f2359332f20adf633a632eaa2a2b4aa22
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-11 20:07:57 +0300

    Create types file for items of Sidebar

---
+++ src/widgets/Sidebar/model/items.ts
@@ -0,0 +1,31 @@
+import React from "react";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+
+import AboutIcon from "shared/assets/images/about-20-20.svg"
+import MainIcon from "shared/assets/images/main-20-20.svg"
+import ProfileIcon from "shared/assets/images/profile-20-20.svg"
+
+export interface SideBarItemType {
+    path: string;
+    text: string;
+    Icon: React.VFC<React.SVGProps<SVGSVGElement>>;
+}
+
+export const SidebarItemsList: SideBarItemType[] = [
+    {
+        path: RoutePath.main,
+        Icon: MainIcon,
+        text: "Главная"
+    },
+    {
+        path: RoutePath.about,
+        Icon: AboutIcon,
+        text: "О сайте"
+    },
+    {
+        path: RoutePath.profile,
+        Icon: ProfileIcon,
+        text: "Профиль"
+    },
+
+]

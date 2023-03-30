c35edadb5ae3750b082ef610e134c1723d70d5db
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:01:51 +0300

    Add slices for profile

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,7 @@
-import {createSlice} from "@reduxjs/toolkit"
-import {ProfileSchema} from "../types/profile";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit"
+import {Profile, ProfileSchema} from "../types/profile";
+import {fetchProfileData} from "../services/fetchProfileData/fetchProfileData";
+import {updateProfileData} from "../services/updateProfileData/updateProfileData";
 
 const initialState: ProfileSchema = {
     readonly: true,
@@ -11,7 +13,67 @@ const initialState: ProfileSchema = {
 export const profileSlice = createSlice({
     name: "profile",
     initialState,
-    reducers: {},
+    reducers: {
+        setReadonly: (state, action: PayloadAction<boolean>) => {
+            state.readonly = action.payload
+        },
+        cancelEdit: (state) => {
+            state.readonly = true;
+            state.validateErrors = undefined;
+            state.form = state.data; // После отмены -> ставим значения с сервера
+        },
+        updateProfile: (state, action: PayloadAction<Profile>) => {
+            state.form = {
+                ...state.data,
+                ...action.payload
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProfileData.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(
+                fetchProfileData.fulfilled, (
+                    state,
+                    action: PayloadAction<Profile>
+                ) => {
+
+                    state.isLoading = false;
+                    state.data = action.payload;
+                    state.form = action.payload;
+                })
+
+            .addCase(fetchProfileData.rejected, (state,
+                action) => {
+                state.isLoading = false;
+                state.error = action.payload
+            })
+            .addCase(updateProfileData.pending, (state) => {
+                state.validateErrors = undefined;
+                state.isLoading = true;
+            })
+            .addCase(
+                updateProfileData.fulfilled, (
+                    state,
+                    action: PayloadAction<Profile>
+                ) => {
+
+                    state.isLoading = false;
+                    state.data = action.payload;
+                    state.form = action.payload;
+                    state.readonly = true;
+                    state.validateErrors = undefined;
+                })
+
+            .addCase(updateProfileData.rejected, (state,
+                action) => {
+                state.isLoading = false;
+                state.validateErrors = action.payload
+            })
+    },
 })
 
 // Action creators are generated for each case reducer function

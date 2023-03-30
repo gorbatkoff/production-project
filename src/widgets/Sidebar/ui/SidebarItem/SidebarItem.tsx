1f17bbbf6bd842a882f9273f6bd32454d361bd02
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:03:59 +0300

    Add condition to render pages by authData

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -5,6 +5,8 @@ import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 import {SideBarItemType} from "../../model/items";
 import {memo} from "react";
 import {classNames} from "shared/lib/classNames/classNames";
+import {useSelector} from "react-redux";
+import {getUserAuthData} from "entities/User";
 
 interface SidebarItemProps {
     item: SideBarItemType;
@@ -14,6 +16,12 @@ interface SidebarItemProps {
 export const SidebarItem = memo(({item, collapsed}: SidebarItemProps) => {
 
     const {t} = useTranslation();
+
+    const isAuth = useSelector(getUserAuthData)
+
+    if(item.authOnly && !isAuth) {
+        return null;
+    }
 
     return (
         <AppLink

e8b5542519721a74799eea28038c22a3e33543ae
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:01:00 +0300

    Create enum of profile erros

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,19 +1,30 @@
-import {Country, Currency} from "shared/const/common";
+import {Currency} from "entities/Currency";
+import {Country} from "entities/Country/model/types/country";
+
+export enum ValidateProfileError {
+    INCORRECT_USER_DATA = "INCORRECT_USER_DATA",
+    INCORRECT_AGE = "INCORRECT_AGE",
+    INCORRECT_COUNTRY = "INCORRECT_COUNTRY",
+    NO_DATA = "NO_DATA",
+    SERVER_ERROR = "SERVER_ERROR",
+}
 
 export interface Profile {
-    first: string,
-    lastname: string,
-    age: number,
-    currency: Currency,
-    country: Country,
-    city: string,
-    username: string,
-    avatar: string
+    first?: string,
+    lastname?: string,
+    age?: number,
+    currency?: Currency,
+    country?: Country,
+    city?: string,
+    username?: string,
+    avatar?: string
 }
 
 export interface ProfileSchema {
     data?: Profile;
+    form?: Profile;
     isLoading: boolean;
     error?: string;
     readonly:  boolean;
+    validateErrors?: ValidateProfileError[];
 }

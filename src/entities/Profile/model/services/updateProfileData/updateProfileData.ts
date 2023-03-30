8080ea00b20bffc1a83eb4bc481d28089e003540
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:13:56 +0300

    Create updating of profile data

---
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -0,0 +1,36 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {ThunkConfig} from "app/providers/StoreProvider";
+import {Profile, ValidateProfileError} from "../../types/profile";
+import {getProfileForm} from "../../selectors/getProfileForm/getProfileForm";
+import {validateProfileData} from "../validateProfileData/validateProfileData";
+
+export const updateProfileData = createAsyncThunk<Profile,
+    void,
+    ThunkConfig<ValidateProfileError[]>>(
+        "profile/updateProfileData",
+        async (_, thunkApi) => {
+            const {
+                extra,
+                rejectWithValue,
+                getState
+            } = thunkApi
+
+            // Получаем данные с формы и отправляем на бэкенд
+            const formData = getProfileForm(getState())
+
+            const errors = validateProfileData(formData);
+
+            if (errors.length) {
+                return rejectWithValue(errors)
+            }
+
+            try {
+                const response = await extra.api.put<Profile>("/profile", formData);
+
+                return response.data
+            } catch (error) {
+                console.log(error)
+                return rejectWithValue([ValidateProfileError.SERVER_ERROR])
+            }
+        }
+    )

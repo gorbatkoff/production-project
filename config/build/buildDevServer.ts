48cf2d493f8e1f7a0fabb481d7b11605d9471fa2
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 11:50:03 +0300

    Del "type"

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,6 +1,6 @@
-import {type BuildOptions} from "./types/config"
+import {BuildOptions} from "./types/config"
 
-import {type Configuration as DevServerConfiguration} from "webpack-dev-server"
+import {Configuration as DevServerConfiguration} from "webpack-dev-server"
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {

b19f6d260c3e9e3670d8f74b69acd3d13403a6a8
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:05:45 +0300

    Add useNavigate

--- src/app/providers/StoreProvider/ui/Storeprovider.tsx
+++ src/app/providers/StoreProvider/ui/Storeprovider.tsx
@@ -2,7 +2,8 @@ import {ReactNode} from "react";
 import {Provider} from "react-redux";
 import {createReduxStore} from "../config/store";
 import {StateSchema} from "../config/StateSchema";
-import {DeepPartial, ReducersMapObject} from "@reduxjs/toolkit";
+import {ReducersMapObject} from "@reduxjs/toolkit";
+import {useNavigate} from "react-router-dom";
 
 interface StoreProviderProps {
     children?: ReactNode;
@@ -12,9 +13,12 @@ interface StoreProviderProps {
 
 export const StoreProvider = ({children, initialState, asyncReducers}: StoreProviderProps) => {
 
+    const navigate = useNavigate()
+
     const store = createReduxStore(
         initialState as StateSchema,
-        asyncReducers as ReducersMapObject<StateSchema>
+        asyncReducers as ReducersMapObject<StateSchema>,
+        navigate
     );
 
     return (

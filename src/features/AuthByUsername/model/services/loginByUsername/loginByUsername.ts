228a96c457062d008e14d8b39a2b774f9959a601
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 11:59:04 +0300

    Add destructuring and new logic

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,31 +1,36 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import axios from "axios";
-import { User } from "entities/User";
-import i18n from "i18next";
+import {User, userActions} from "entities/User";
 import {USER_LOCALSTORAGE_KEY} from "shared/const/localStorage";
-import {userActions} from "entities/User";
+import {ThunkConfig} from "app/providers/StoreProvider";
 
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>(
-    "login/loginByUsername",
-    async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post<User>("http://localhost:8000/login", authData)
+export const loginByUsername = createAsyncThunk<
+    User,
+    LoginByUsernameProps,
+    ThunkConfig<string>
+    >(
+        "login/loginByUsername",
+        async (authData, thunkApi) => {
+            const {
+                extra,
+                dispatch,
+                rejectWithValue
+            } = thunkApi
+            try {
+                const response = await extra.api.post<User>("/login", authData)
+                if (!response.data) throw new Error();
 
-            if(!response.data) throw new Error();
+                localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+                dispatch(userActions.setAuthData(response.data));
 
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
-
-            return response.data
-
-        } catch (error) {
-            console.log(error)
-            return thunkAPI.rejectWithValue("Error")
+                return response.data
+            } catch (error) {
+                console.log(error)
+                return rejectWithValue("Error")
+            }
         }
-    }
-)
+    )

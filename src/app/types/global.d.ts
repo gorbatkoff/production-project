6fa140e869029f4def45fa397db8aa82f314ac4f
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 11:55:09 +0300

    Add global variable and typed DeepPartial

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -16,3 +16,8 @@ declare module "*.svg" {
 }
 
 declare const __IS_DEV__: boolean
+declare const __API_URL__: string
+
+type DeepPartial<T> = T extends object ? {
+	[P in keyof T]?: DeepPartial<T[P]>;
+} : T;

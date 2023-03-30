7949502842c915b059169c5d52bc0f2f74668ef4
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 11:53:13 +0300

    Add apiUrl to BuildOptions and BuildEnv

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -10,6 +10,7 @@ export type BuildPaths = {
 export type BuildEnv = {
 	mode: BuildMode;
 	port: number;
+	apiUrl: string;
 };
 
 export type BuildOptions = {
@@ -17,4 +18,5 @@ export type BuildOptions = {
 	paths: BuildPaths;
 	isDev: boolean;
 	port: number;
+	apiUrl: string;
 };

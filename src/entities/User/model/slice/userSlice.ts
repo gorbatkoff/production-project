90d7e7f4a5447bed1c51b5f5aa3ad06e8d036028
gorbatkoff <gorbatkovartyom@gmail.com>
2023-03-30 12:07:05 +0300

    Delete useless action.payload

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -2,24 +2,24 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit"
 import {User, UserSchema} from "../types/user";
 import {USER_LOCALSTORAGE_KEY} from "shared/const/localStorage";
 
-const initialState:UserSchema = {}
+const initialState: UserSchema = {}
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        setAuthData: (state, action:PayloadAction<User>) => {
+        setAuthData: (state, action: PayloadAction<User>) => {
             state.authData = action.payload
         },
 
-        initAuthData: (state, action:PayloadAction<User>) => {
+        initAuthData: (state) => {
             const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
-            if(user){
+            if (user) {
                 state.authData = JSON.parse(user)
             }
         },
 
-        logout: (state, action:PayloadAction<User>) => {
+        logout: (state) => {
             state.authData = undefined;
             localStorage.removeItem(USER_LOCALSTORAGE_KEY)
         },

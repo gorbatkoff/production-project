0159e78d107b50fc2ced75db6ef8b2c566a91d88
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-08 13:45:52 +0300

    Update screenshots and stories

--- src/shared/ui/Select/Select.stories.tsx
+++ src/shared/ui/Select/Select.stories.tsx
@@ -1,27 +1,22 @@
 import React from "react";
-import {ComponentMeta, ComponentStory} from "@storybook/react";
-import {Select} from "./Select";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Select } from "shared/ui/Select/Select";
 
 export default {
     title: "shared/Select",
     component: Select,
     argTypes: {
-        backgroundColor: {control: "color"},
+        backgroundColor: { control: "color" },
     },
-    arg: {
-        to: "/"
-    }
 } as ComponentMeta<typeof Select>;
 
 const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
 
-export const SelectDefault = Template.bind({});
-SelectDefault.args = {
-    label: "Укажите значения",
+export const Primary = Template.bind({});
+Primary.args = {
+    label: "Укажите значение",
     options: [
-        {value: "123", content: "Первый пункт"},
-        {value: "1234", content: "Второй пункт"},
-        {value: "12345", content: "Третий пункт"},
-    ]
+        { value: "123", content: "Первый пункт" },
+        { value: "1234", content: "Второй пункт" },
+    ],
 };
-

08d4bf349ca7f493132e67e15e7eb50c48574ba4
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-22 01:41:48 +0300

    Add new route

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -26,7 +26,7 @@ export enum AppRoutes {
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: "/",
     [AppRoutes.ABOUT]: "/about",
-    [AppRoutes.PROFILE]: "/profile",
+    [AppRoutes.PROFILE]: "/profile/", // + :id
     [AppRoutes.ARTICLES]: "/articles",
     [AppRoutes.ARTICLES_DETAILS]: "/articles/", // + :id
     [AppRoutes.NOT_FOUND]: "*",
@@ -42,7 +42,7 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
         element: <AboutPage/>,
     },
     [AppRoutes.PROFILE]: {
-        path: RoutePath.profile,
+        path: `${RoutePath.profile}:id`,
         element: <ProfilePage/>,
         authOnly: true
     },

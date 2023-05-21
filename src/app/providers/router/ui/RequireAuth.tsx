eda7001a9a58685b6abdde17f866ac2dfebc4b0a
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-21 13:13:57 +0300

    Del comments

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -8,10 +8,6 @@ export default function RequireAuth({children}: { children: JSX.Element }) {
     const location = useLocation();
 
     if (!auth) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to when they were redirected. This allows us to send them
-        // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
         return <Navigate to={RoutePath.main} state={{from: location}} replace/>;
     }
 

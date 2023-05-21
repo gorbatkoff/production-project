42f8554fd9733a38d6cb1d3f6051914c9d77eb8f
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-22 01:42:01 +0300

    Add new reducer

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -7,6 +7,7 @@ import {AxiosInstance} from "axios";
 import {NavigateOptions, To} from "react-router-dom";
 import {ArticleDetailsSchema} from "entities/Article";
 import {ArticleDetailsCommentsSchema} from "pages/ArticleDetailsPage";
+import { AddCommentFormSchema } from "features/addCommentForm";
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -17,6 +18,7 @@ export interface StateSchema {
     loginForm?: LoginSchema;
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
+    addCommentForm?: AddCommentFormSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;

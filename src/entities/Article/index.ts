2e5ff1bc2ffc9de48b182f67b450a1d99dbeda61
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-22 01:35:35 +0300

    Public API

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -4,3 +4,4 @@ export {
 
 export type {Article} from "./model/types/article"
 export type {ArticleDetailsSchema} from "./model/types/articleDetailsSchema"
+export {getArticleDetailsData} from "./model/selectors/articleDetails"

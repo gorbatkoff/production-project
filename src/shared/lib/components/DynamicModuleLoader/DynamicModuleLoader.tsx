672b25a2ee6878bbdd280cefbb68c6d574171477
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-22 01:40:35 +0300

    Default value for removeAfterUnmount

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -14,7 +14,11 @@ interface DynamicModuleLoaderProps {
 
 export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
 
-    const {children, reducers, removeAfterUnmount} = props;
+    const {
+        children,
+        reducers,
+        removeAfterUnmount = true
+    } = props;
 
     const store = useStore() as ReduxStoreWithManager;
     const dispatch = useDispatch()

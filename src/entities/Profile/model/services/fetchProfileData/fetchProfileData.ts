4d72b5a394ad2080afa5d7b5bf1677b24b194ecf
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-22 01:43:13 +0300

    Change endpoint for API request

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -3,16 +3,16 @@ import {ThunkConfig} from "app/providers/StoreProvider";
 import {Profile} from "../../types/profile";
 
 
-export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+export const fetchProfileData = createAsyncThunk<Profile, string, ThunkConfig<string>>(
     "profile/fetchProfileData",
-    async (_, thunkApi) => {
+    async (profileId, thunkApi) => {
         const {
             extra,
             rejectWithValue
         } = thunkApi
 
         try {
-            const response = await extra.api.get<Profile>("/profile")
+            const response = await extra.api.get<Profile>("/profile/" + profileId)
 
             if(!response.data) {
                 throw new Error();

e9a3e17cbfbcfab9eba48cee3fda5c3f3ddeb4cb
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-21 13:10:27 +0300

    Public API

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,7 @@
 export {
     ArticleDetailsPageAsync as ArticleDetailsPage
 } from "./ui/ArticleDetailsPage/ArticleDetailsPage.async"
+
+export {
+    ArticleDetailsCommentsSchema
+} from "./model/types/ArticleDetailsCommentsSchema"

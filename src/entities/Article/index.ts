852070b944cc5a3e71cd0dc7fa8f8dec6d827c19
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-26 18:02:37 +0300

    Update Public API

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -2,6 +2,8 @@ export {
     ArticleDetails
 } from "./ui/ArticleDetails/ArticleDetails"
 
-export type {Article} from "./model/types/article"
-export type {ArticleDetailsSchema} from "./model/types/articleDetailsSchema"
+export {Article, ArticleView} from "./model/types/article"
+export {ArticleDetailsSchema} from "./model/types/articleDetailsSchema"
 export {getArticleDetailsData} from "./model/selectors/articleDetails"
+
+export {ArticleList} from "./ui/ArticleList/ArticleList"

4daf64e15fa530ab45d2621bb37fc423c0cba946
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-27 13:20:51 +0300

    Update Public API

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -7,3 +7,5 @@ export {ArticleDetailsSchema} from "./model/types/articleDetailsSchema"
 export {getArticleDetailsData} from "./model/selectors/articleDetails"
 
 export {ArticleList} from "./ui/ArticleList/ArticleList"
+
+export {ArticleViewSelector} from "./ui/ArticleViewSelector/ArticleViewSelector"
--- src/pages/ArticlesPage/index.ts
+++ src/pages/ArticlesPage/index.ts
@@ -1,3 +1,6 @@
 export {
     ArticlesPageAsync as ArticlesPage
 } from "./ui/ArticlesPage/ArticlesPage.async"
+
+// Для того, чтобы получить его в StateSchena
+export {ArticlePageSchema} from './model/types/articlePageSchema'

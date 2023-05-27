59023e8d299071e72eaa11b61377afb16c5042ec
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-27 13:24:25 +0300

    Add fetching list of articles

---
+++ src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
@@ -0,0 +1,32 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {ThunkConfig} from "app/providers/StoreProvider";
+import {Article} from "entities/Article";
+
+export const fetchArticlesList = createAsyncThunk<
+    Article[],
+    void,
+    ThunkConfig<string>>(
+        "articlePage/fetchArticlesList",
+        async (articleId, thunkApi) => {
+            const {
+                extra,
+                rejectWithValue
+            } = thunkApi
+
+            try {
+                const response = await extra.api.get<Article[]>("/articles/", {
+                    params: {
+                        _expand: "user"
+                    }
+                })
+
+                if (!response.data) {
+                    throw new Error();
+                }
+
+                return response.data
+            } catch (error) {
+                return rejectWithValue("Error")
+            }
+        }
+    )

bcf467c3bcf1cf9cdbac89529cff9fb9f4d2fefd
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-27 13:22:54 +0300

    Add new reducer (articlePage)

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -8,6 +8,7 @@ import {NavigateOptions, To} from "react-router-dom";
 import {ArticleDetailsSchema} from "entities/Article";
 import {ArticleDetailsCommentsSchema} from "pages/ArticleDetailsPage";
 import { AddCommentFormSchema } from "features/addCommentForm";
+import { ArticlePageSchema } from "pages/ArticlesPage";
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -19,6 +20,7 @@ export interface StateSchema {
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
     addCommentForm?: AddCommentFormSchema;
+    articlePage?: ArticlePageSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;

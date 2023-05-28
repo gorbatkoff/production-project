2a491382b52890ae480eaa629000d64463e0cef5
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 13:52:38 +0300

    Add fields for pagination

--- src/pages/ArticlesPage/model/types/articlePageSchema.ts
+++ src/pages/ArticlesPage/model/types/articlePageSchema.ts
@@ -5,4 +5,8 @@ export interface ArticlePageSchema extends EntityState<Article> {
     isLoading?: boolean,
     error?: string,
     view: ArticleView,
+    // Pagination
+    page: number,
+    limit?: number,
+    hasMore: boolean
 }

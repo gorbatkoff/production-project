3f415ca23d50cc3e6464b24f58d2fbb88442b9fd
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 13:55:27 +0300

    Async thunk for fetching articles list

--- src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
+++ src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
@@ -1,22 +1,33 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {ThunkConfig} from "app/providers/StoreProvider";
 import {Article} from "entities/Article";
+import {getArticlePageLimit} from "../../selectors/articlesPageSelectors";
+
+interface FetchArticlesListProps {
+    page?: number;
+}
 
 export const fetchArticlesList = createAsyncThunk<
     Article[],
-    void,
+    FetchArticlesListProps,
     ThunkConfig<string>>(
         "articlePage/fetchArticlesList",
-        async (articleId, thunkApi) => {
+        async (props, thunkApi) => {
             const {
                 extra,
-                rejectWithValue
+                rejectWithValue,
+                getState
             } = thunkApi
+            const {page = 1} = props;
+
+            const limit = getArticlePageLimit(getState())
 
             try {
                 const response = await extra.api.get<Article[]>("/articles/", {
                     params: {
-                        _expand: "user"
+                        _expand: "user",
+                        _limit: limit,
+                        _page: page
                     }
                 })
 

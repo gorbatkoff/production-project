10dc77dd8f07b49eaa17cd1d5cae4785b96f5120
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 13:54:16 +0300

    New selectors

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -4,3 +4,6 @@ import {ArticleView} from "entities/Article";
 export const getArticlePageError = (state: StateSchema) => state.articlePage?.error
 export const getArticlePageIsLoading = (state: StateSchema) => state.articlePage?.isLoading
 export const getArticlePageView = (state: StateSchema) => state.articlePage?.view || ArticleView.LIST
+export const getArticlePageNum = (state: StateSchema) => state.articlePage?.page || 1;
+export const getArticlePageHasMore = (state: StateSchema) => state.articlePage?.hasMore;
+export const getArticlePageLimit = (state: StateSchema) => state.articlePage?.limit || 9

fcd5d5e08abf850f63313d319ae32a3763f1de70
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 13:51:30 +0300

    Change import of mocked article

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
@@ -3,7 +3,7 @@ import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 import {ArticleListItem} from "./ArticleListItem";
 import {ArticleView} from "entities/Article";
-import {article} from "pages/ArticlesPage/ui/ArticlesPage/ArticlesPage";
+import { article } from "../ArticleList/ArticleList.stories";
 
 export default {
     title: "entities/ArticleListItem",

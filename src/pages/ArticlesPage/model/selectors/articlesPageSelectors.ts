abba232f707a93432578f8effa78e8f21c7faafe
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 17:48:53 +0300

    New selectors and default value

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -2,8 +2,9 @@ import {StateSchema} from "app/providers/StoreProvider";
 import {ArticleView} from "entities/Article";
 
 export const getArticlePageError = (state: StateSchema) => state.articlePage?.error
-export const getArticlePageIsLoading = (state: StateSchema) => state.articlePage?.isLoading
-export const getArticlePageView = (state: StateSchema) => state.articlePage?.view || ArticleView.LIST
+export const getArticlePageIsLoading = (state: StateSchema) => state.articlePage?.isLoading || false
+export const getArticlePageView = (state: StateSchema) => state.articlePage?.view || ArticleView.TILE
 export const getArticlePageNum = (state: StateSchema) => state.articlePage?.page || 1;
 export const getArticlePageHasMore = (state: StateSchema) => state.articlePage?.hasMore;
 export const getArticlePageLimit = (state: StateSchema) => state.articlePage?.limit || 9
+export const getArticlePageInited = (state: StateSchema) => state.articlePage?._inited || false;

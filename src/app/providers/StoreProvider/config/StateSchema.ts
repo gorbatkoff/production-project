ce0ef3b9f5e560a95232ee9ef57ae005ade3b288
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-28 17:50:39 +0300

    New fields and types

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -7,8 +7,8 @@ import {AxiosInstance} from "axios";
 import {NavigateOptions, To} from "react-router-dom";
 import {ArticleDetailsSchema} from "entities/Article";
 import {ArticleDetailsCommentsSchema} from "pages/ArticleDetailsPage";
-import { AddCommentFormSchema } from "features/addCommentForm";
-import { ArticlePageSchema } from "pages/ArticlesPage";
+import {AddCommentFormSchema} from "features/addCommentForm";
+import {ArticlePageSchema} from "pages/ArticlesPage";
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -24,12 +24,14 @@ export interface StateSchema {
 }
 
 export type StateSchemaKey = keyof StateSchema;
+export type MountedReducers = OptionalRecord<StateSchemaKey, boolean>;
 
 export interface ReducerManager {
     getReducerMap: () => ReducersMapObject<StateSchema>;
     reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>
     add: (key: StateSchemaKey, reducer: Reducer) => void;
     remove: (key: StateSchemaKey) => void;
+    getMountedReducers: () => MountedReducers;
 }
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
@@ -38,7 +40,6 @@ export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
 
 export interface ThunkExtraArg {
     api: AxiosInstance,
-    navigate?: (to: To, options?: NavigateOptions) => void
 }
 
 export interface ThunkConfig<T> {

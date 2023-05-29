1b3891f6e9b7a65735886b12caef7beb21d10ebf
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-29 11:51:06 +0300

    Add test for async thunk

---
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.test.ts
@@ -0,0 +1,53 @@
+import {initArticlesPage} from "./initArticlesPage";
+import {TestAsyncThunk} from "shared/lib/tests/TestAsyncThunk/TestAsyncThunk";
+import {fetchArticlesList} from "../../services/fetchArticlesList/fetchArticlesList";
+
+jest.mock("../fetchArticlesList/fetchArticlesList")
+
+describe("fetchNextArticlesPage.test", () => {
+    test("Success", async () => {
+        const thunk = new TestAsyncThunk(initArticlesPage, {
+            articlePage: {
+                page: 2,
+                hasMore: true,
+                limit: 5,
+                ids: [],
+                entities: {},
+                isLoading: false,
+                _inited: false
+            }
+        });
+
+        await thunk.callThunk()
+
+        // Pending + Fulfilled + 2 dispatches
+        expect(thunk.dispatch).toBeCalledTimes(4)
+        expect(fetchArticlesList).toHaveBeenCalledWith({
+            page: 1
+        })
+    });
+
+})
+
+describe("fetchNextArticlesPage.test", () => {
+    test("Success", async () => {
+        const thunk = new TestAsyncThunk(initArticlesPage, {
+            articlePage: {
+                page: 2,
+                hasMore: true,
+                limit: 5,
+                ids: [],
+                entities: {},
+                isLoading: false,
+                _inited: true
+            }
+        });
+
+        await thunk.callThunk()
+
+        // Pending + Fulfilled
+        expect(thunk.dispatch).toBeCalledTimes(2)
+        expect(thunk.dispatch).not.toBeCalledTimes(3)
+    });
+
+})

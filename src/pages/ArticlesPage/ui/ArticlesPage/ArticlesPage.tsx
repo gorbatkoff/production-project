3ce5bd712ed5e9d47f0f9a38db5145bc398cf6d9
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-29 11:47:47 +0300

    Change import

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -13,7 +13,7 @@ import {
     getArticlePageNum,
     getArticlePageView
 } from "../../model/selectors/articlesPageSelectors";
-import {Page} from "shared/ui/Page/Page";
+import {Page} from "widgets/Page/Page";
 import {fetchNextArticlesPage} from "../../model/services/fetchNextArticlesPage/fetchNextArticlesPage";
 import {initArticlesPage} from "../../model/services/initArticlesPage/initArticlesPage";
 

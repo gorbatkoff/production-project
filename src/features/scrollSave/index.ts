b3ef8896da0b50b83589e868e6973f33748d12ba
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-29 11:52:00 +0300

    Public API

---
+++ src/features/scrollSave/index.ts
@@ -0,0 +1,7 @@
+export {ScrollSaveSchema} from "./model/types/scrollSave"
+
+export {
+    getScrollByPath
+} from "./model/selectors/scrollSaveSelectror"
+
+export {scrollSaveReducer, scrollSaveActions} from "./model/slice/scrollSaveSlice"

6201de5cbada46116704f894de65c9e55ce79823
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-29 11:47:40 +0300

    Change import

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -16,7 +16,7 @@ import AddCommentForm from "features/addCommentForm/ui/AddCommentForm/AddComment
 import {addCommentForArticle} from "../../model/services/addCommentForArticle/addCommentForArticle";
 import {Button, ButtonTheme} from "shared/ui/Button/Button";
 import {RoutePath} from "shared/config/routeConfig/routeConfig";
-import {Page} from "shared/ui/Page/Page";
+import {Page} from "widgets/Page/Page";
 
 interface ArticleDetailsPageProps {
     className?: string

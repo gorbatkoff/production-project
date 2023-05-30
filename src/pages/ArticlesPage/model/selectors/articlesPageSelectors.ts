005bf6fdbb581dabb984b1f7b6960f3aeabe91a0
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-30 23:38:22 +0300

    New selectors

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -1,5 +1,5 @@
 import {StateSchema} from "app/providers/StoreProvider";
-import {ArticleView} from "entities/Article";
+import {ArticleSortField, ArticleTypes, ArticleView} from "entities/Article";
 
 export const getArticlePageError = (state: StateSchema) => state.articlePage?.error
 export const getArticlePageIsLoading = (state: StateSchema) => state.articlePage?.isLoading || false
@@ -8,3 +8,7 @@ export const getArticlePageNum = (state: StateSchema) => state.articlePage?.page
 export const getArticlePageHasMore = (state: StateSchema) => state.articlePage?.hasMore;
 export const getArticlePageLimit = (state: StateSchema) => state.articlePage?.limit || 9
 export const getArticlePageInited = (state: StateSchema) => state.articlePage?._inited || false;
+export const getArticlePageOrder = (state: StateSchema) => state.articlePage?.order ?? "asc";
+export const getArticlePageSort = (state: StateSchema) => state.articlePage?.sort ?? ArticleSortField.CREATED;
+export const getArticlePageSearch = (state: StateSchema) => state.articlePage?.search ?? "";
+export const getArticlePageType = (state: StateSchema) => state.articlePage?.type || ArticleTypes.ALL;

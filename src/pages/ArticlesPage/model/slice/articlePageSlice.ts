f7ce666f961685f848b80d314607e229f36bbfc2
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-30 23:39:31 +0300

    New slice and schema

--- src/pages/ArticlesPage/model/slice/articlePageSlice.ts
+++ src/pages/ArticlesPage/model/slice/articlePageSlice.ts
@@ -1,9 +1,11 @@
 import {createEntityAdapter, createSlice, PayloadAction,} from "@reduxjs/toolkit";
 import {StateSchema} from "app/providers/StoreProvider";
-import {Article, ArticleView} from "entities/Article";
-import {ArticlePageSchema} from "pages/ArticlesPage";
+import {Article, ArticleTypes, ArticleView} from "entities/Article";
+import {ArticlePageSchema} from "../types/articlePageSchema";
 import {fetchArticlesList} from "../services/fetchArticlesList/fetchArticlesList";
 import {ARTICLEVIEW_LOCALSTORAGE_KEY} from "shared/const/localStorage";
+import {ArticleSortField} from "entities/Article";
+import {SortOrder} from "shared/types";
 
 const articlesAdapter = createEntityAdapter<Article>({
     selectId: (article) => article.id,
@@ -22,7 +24,12 @@ const articlesPageSlice = createSlice({
         view: ArticleView.TILE,
         page: 1,
         hasMore: true,
-        _inited: false
+        _inited: false,
+        limit: 9,
+        order: "asc",
+        search: "",
+        sort: ArticleSortField.CREATED,
+        type: ArticleTypes.ALL
     }),
     reducers: {
         setView: (state, action: PayloadAction<ArticleView>) => {
@@ -33,6 +40,18 @@ const articlesPageSlice = createSlice({
         setPage: (state, action: PayloadAction<number>) => {
             state.page = action.payload
         },
+        setOrder: (state, action: PayloadAction<SortOrder>) => {
+            state.order = action.payload
+        },
+        setSearch: (state, action: PayloadAction<string>) => {
+            state.search = action.payload
+        },
+        setSort: (state, action: PayloadAction<ArticleSortField>) => {
+            state.sort = action.payload
+        },
+        setType: (state, action: PayloadAction<ArticleTypes>) => {
+            state.type = action.payload
+        },
         initState: (state) => {
             const view = localStorage.getItem(ARTICLEVIEW_LOCALSTORAGE_KEY) as ArticleView;
             state.view = view;
@@ -41,19 +60,27 @@ const articlesPageSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchArticlesList.pending, (state) => {
+            .addCase(fetchArticlesList.pending, (state, action) => {
                 state.error = undefined;
                 state.isLoading = true;
+
+                if(action.meta.arg.replace) {
+                    articlesAdapter.removeAll(state);
+                }
             })
             .addCase(
                 fetchArticlesList.fulfilled, (
                     state,
-                    action: PayloadAction<Article[]>
+                    action
                 ) => {
                     state.isLoading = false;
-                    articlesAdapter.addMany(state, action.payload);
-                    state.hasMore = action.payload.length > 0;
-                    state._inited = true
+                    state.hasMore = action.payload.length >= state.limit;
+
+                    if (action.meta.arg.replace) {
+                        articlesAdapter.setAll(state, action.payload);
+                    } else {
+                        articlesAdapter.addMany(state, action.payload);
+                    }
                 })
 
             .addCase(fetchArticlesList.rejected,
--- src/pages/ArticlesPage/model/types/articlePageSchema.ts
+++ src/pages/ArticlesPage/model/types/articlePageSchema.ts
@@ -1,13 +1,22 @@
 import {EntityState} from "@reduxjs/toolkit";
 import {Article, ArticleView} from "entities/Article";
+import {SortOrder} from "shared/types";
+import {ArticleSortField, ArticleTypes} from "entities/Article";
 
 export interface ArticlePageSchema extends EntityState<Article> {
     isLoading?: boolean,
     error?: string,
-    view: ArticleView,
     // Pagination
     page: number,
-    limit?: number,
+    limit: number,
     hasMore: boolean,
+
+    //filters
+    view: ArticleView,
+    order: SortOrder,
+    sort: ArticleSortField; // То, по чему мы можем сортировать дата, название, просмотры
+    search: string,
+    type: ArticleTypes
+
     _inited: boolean,
 }

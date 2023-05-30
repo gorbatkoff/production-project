b5382dca2209a3179e7a46bdefd2dd0d1948890e
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-30 23:37:30 +0300

    Public API

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -2,10 +2,11 @@ export {
     ArticleDetails
 } from "./ui/ArticleDetails/ArticleDetails"
 
-export {Article, ArticleView} from "./model/types/article"
+export {Article, ArticleView, ArticleSortField, ArticleTypes} from "./model/types/article"
 export {ArticleDetailsSchema} from "./model/types/articleDetailsSchema"
 export {getArticleDetailsData} from "./model/selectors/articleDetails"
 
 export {ArticleList} from "./ui/ArticleList/ArticleList"
-
 export {ArticleViewSelector} from "./ui/ArticleViewSelector/ArticleViewSelector"
+export {ArticleSortSelector} from "./ui/ArticleSortSelector/ArticleSortSelector"
+export {ArticleTypeTabs} from "./ui/ArticleTypeTabs/ArticleTypeTabs"

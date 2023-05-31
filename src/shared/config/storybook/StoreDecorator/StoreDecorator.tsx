03bfabd62ae9d02f89d20056fc3bc529664c333a
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-31 13:13:50 +0300

    Update schemas and selectors

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -5,14 +5,14 @@ import {profileReducer} from "entities/Profile";
 import {ReducersList} from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 import {articleDetailsReducer} from "entities/Article/model/slice/articleDetailsSlice";
 import {addCommentFormReducer} from "features/addCommentForm/model/slice/addCommentFormSlice";
-import {articleDetailsCommentsReducer} from "pages/ArticleDetailsPage/model/slices/ArticleDetailsCommentsSlice";
+import {articleDetailsPageReducer} from "pages/ArticleDetailsPage/model/slices";
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
     profile: profileReducer,
     articleDetails: articleDetailsReducer,
     addCommentForm: addCommentFormReducer,
-    articleDetailsComments: articleDetailsCommentsReducer
+    articlesDetailsPage: articleDetailsPageReducer
 }
 
 export const StoreDecorator = (

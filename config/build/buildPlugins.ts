c2ff3ecb6dd5fb5b4f98f6656634ae298676bb78
gorbatkoff <gorbatkovartyom@gmail.com>
2023-05-31 18:04:02 +0300

    Using CopyPlugin add translations to build folder

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,6 +6,7 @@ import {BuildOptions} from "./types/config"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer"
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
 
@@ -22,6 +23,11 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
             __IS_DEV__: JSON.stringify(isDev),
             __API_URL__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
         }),
     ]
 
